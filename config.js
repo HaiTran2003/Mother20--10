2534eb401b5c7e73cf850038ee5a3e42302fec1d
HaiTran2003 <126803061+HaiTran2003@users.noreply.github.com>
2023-10-20 21:12:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Mother';
+const giftUrl = 'https://www.facebook.com/lan.nhu.5015';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Mẹ Iu';
+const contentCard = 'Mẹ yêu dấu! Thật buồn vì con không ở bên mẹ vào ngày đặc biệt này. Con chúc mẹ có một ngày 20/10 thật vui vẻ, hạnh phúc. Mẹ hãy luôn mỉm cười và sống thật lâu mẹ nhé. Con sẽ sớm về nhà với mẹ thôi. Con yêu mẹ rất nhiều!';
+
+const giftImage = 'Me-Iu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
